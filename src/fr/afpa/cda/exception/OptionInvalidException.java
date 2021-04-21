3b9f4a3a67f00af7e5aec8e8d636f95d9438e2b2
Ludivine <hadesvapeur8@gmail.com>
2021-04-21 15:17:54 +0200

    Création de l'exception OptionInvalidExpetion

---
+++ src/fr/afpa/cda/exception/OptionInvalidException.java
@@ -0,0 +1,36 @@
+package fr.afpa.cda.exception;
+
+public class OptionInvalidException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7853963423734938716L;
+
+	public OptionInvalidException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public OptionInvalidException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public OptionInvalidException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public OptionInvalidException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public OptionInvalidException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
